dcbc4db5885f7365989e66777280e5fdff8ec150
GengYage <icoderdev@outlook.com>
2023-08-13 13:56:40 +0800

    feat(keyboard)PS/2键盘驱动

--- src/drivers/keyboard.rs
+++ src/drivers/keyboard.rs
@@ -1,9 +1,13 @@
-use crate::info;
 use crate::kernel::interrupts::handler::set_interrupt_handler;
 use crate::kernel::interrupts::pic::pic_controller::send_eoi;
 use crate::kernel::interrupts::{
     set_interrupt_mask, IRQ_KEYBOARD, IRQ_MASTER_NR,
 };
+use crate::kernel::sync::mutex::Mutex;
+use crate::print;
+use lazy_static::lazy_static;
+use pc_keyboard::layouts::Us104Key;
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use x86::io::inb;
 
 const KEYBOARD_DATA_PORT: u16 = 0x60;
@@ -38,8 +42,31 @@ pub extern "C" fn keyboard_handler(
         inb(KEYBOARD_DATA_PORT)
     };
 
-    info!("keyboard input {scancode}");
+    // 解析键盘的scancode
+    parser_scancode(scancode);
 }
+
+fn parser_scancode(scancode: u8) {
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                Us104Key,
+                HandleControl::Ignore
+            ));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("RawKey {:?}", key),
+            }
+        }
+    }
+}
+
 pub fn init_keyboard() {
     set_interrupt_handler(
         IRQ_MASTER_NR + IRQ_KEYBOARD as usize,
