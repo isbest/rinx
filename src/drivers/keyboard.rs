9060c415e66c782e2cd69dbb572c213564883c87
GengYage <icoderdev@outlook.com>
2023-08-13 14:48:33 +0800

    feat(keyboard)LED灯驱动

--- src/drivers/keyboard.rs
+++ src/drivers/keyboard.rs
@@ -1,3 +1,8 @@
+use lazy_static::lazy_static;
+use pc_keyboard::layouts::Us104Key;
+use pc_keyboard::{DecodedKey, HandleControl, KeyCode, Keyboard, ScancodeSet1};
+use x86::io::{inb, outb};
+
 use crate::kernel::interrupts::handler::set_interrupt_handler;
 use crate::kernel::interrupts::pic::pic_controller::send_eoi;
 use crate::kernel::interrupts::{
@@ -5,13 +10,12 @@ use crate::kernel::interrupts::{
 };
 use crate::kernel::sync::mutex::Mutex;
 use crate::print;
-use lazy_static::lazy_static;
-use pc_keyboard::layouts::Us104Key;
-use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
-use x86::io::inb;
 
 const KEYBOARD_DATA_PORT: u16 = 0x60;
 const KEYBOARD_CTRL_PORT: u16 = 0x60;
+/// 设置LED状态
+const KEYBOARD_CMD_LED: u8 = 0xED;
+const KEYBOARD_CMD_ACK: u8 = 0xFA;
 
 /// 键盘中断处理函数
 pub extern "C" fn keyboard_handler(
@@ -46,9 +50,13 @@ pub extern "C" fn keyboard_handler(
     parser_scancode(scancode);
 }
 
+/// 键盘大小写锁定
+static mut CAPSLOCK_STATE: bool = false;
+
+/// 解析扫描码,PS/2键盘驱动的关键,利用`pc_keyboard`crate实现
 fn parser_scancode(scancode: u8) {
     lazy_static! {
-        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+        static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> =
             Mutex::new(Keyboard::new(
                 ScancodeSet1::new(),
                 Us104Key,
@@ -61,10 +69,54 @@ fn parser_scancode(scancode: u8) {
         if let Some(key) = keyboard.process_keyevent(key_event) {
             match key {
                 DecodedKey::Unicode(character) => print!("{}", character),
-                DecodedKey::RawKey(key) => print!("RawKey {:?}", key),
+                DecodedKey::RawKey(key) => {
+                    if KeyCode::CapsLock == key {
+                        unsafe {
+                            CAPSLOCK_STATE = !CAPSLOCK_STATE;
+                            set_led(CAPSLOCK_STATE);
+                        }
+                    }
+                }
             }
         }
     }
+}
+
+// 等待缓冲区为空
+#[inline(always)]
+fn keyboard_wait() {
+    let mut state: u8 = 0;
+    while state & 0x02 != 0 {
+        unsafe {
+            state = inb(KEYBOARD_CTRL_PORT);
+        }
+    }
+}
+
+// 等待键盘回复ack
+#[inline(always)]
+fn keyboard_ack() {
+    let mut state: u8 = 0;
+    while state != KEYBOARD_CMD_ACK {
+        unsafe {
+            state = inb(KEYBOARD_DATA_PORT);
+        }
+    }
+}
+
+// 设置键盘LED灯状态
+fn set_led(state: bool) {
+    let led: u8 = if state { 1 << 2 } else { 0 };
+    keyboard_wait();
+    unsafe {
+        outb(KEYBOARD_DATA_PORT, KEYBOARD_CMD_LED);
+    }
+    keyboard_ack();
+    keyboard_wait();
+    unsafe {
+        outb(KEYBOARD_DATA_PORT, led);
+    }
+    keyboard_ack();
 }
 
 pub fn init_keyboard() {
